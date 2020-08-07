7871810f03c9864f10d3b56373949a7818d0991a
morrisonso <sophiemorrison@outlook.co.nz>
2020-08-08 09:17:59 +1200

    tidying up old content, ready for portfolio material

--- assets/js/contactjs.js
+++
@@ -1,8 +0,0 @@
-//contact page form
-const mssgFeedback = () => {
-    const userName = document.getElementById('name').value
-    document.getElementById('feedback')
-    .innerHTML = `Thank you for your message, ${userName}.`
-  }
-  
-  document.getElementById('submitbttn').addEventListener('click', mssgFeedback)
